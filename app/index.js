c398be316dcb38233e4ffeac13f6fe2a5c47ad7a
Alex Boklin <alex.boklin@gmail.com>
2016-07-19 14:22:37 +0300

    rewrote index.js in new style

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
-var React = require('react');
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-var Hello = React.createClass({
-  render: function () {
+export default class Hello extends React.Component {
+  render() {
     return (
       <div>Hello ReactJS Program!</div>
     )
   }
-});
+}
 
 ReactDOM.render(<Hello />, document.getElementById('app'));
