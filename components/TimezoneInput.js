450a663fa031add445ad641d43fb94073bb47c28
Alex Boklin <alex.boklin@gmail.com>
2016-07-20 18:39:38 +0300

    added constructor and handleChange

--- components/TimezoneInput.js
+++ components/TimezoneInput.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 export default class TimezoneInput extends React.Component {
-  render() {
-    return (
-      <div className="col-sm-6 col-sm-offset-3 text-center">
-        <form onSubmit={console.log("onSubmit")}>
-          <input
-            type="text"
-            placeholder="Type in the city"
-            value=""
-            onChange={console.log("onChange")}
-          />
-          <input type="submit" value="Submit"/>
-        </form>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputText: ''
+        }
+    }
+
+    handleChange(event) {
+        this.setState({
+            inputText: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="col-sm-6 col-sm-offset-3 text-center">
+                <form onSubmit={console.log("onSubmit")}>
+                    <input
+                        type="text"
+                        placeholder="Type in the city"
+                        value={this.state.inputText}
+                        onChange={this.handleChange.bind(this)}
+                    />
+                <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        )
+    }
 }
