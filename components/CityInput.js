041b5fbbd166cc0488f3d59731d7b1ef8e241c5d
alexboklin <alex.boklin@gmail.com>
2016-07-22 18:15:50 +0300

    only center TextField, no col classes applied

--- components/CityInput.js
+++ components/CityInput.js
@@ -22,22 +22,19 @@ export default class CityInput extends React.Component {
         // On hitting Enter we add city to the list.
         if (event.keyCode === 13) {
             console.log(this.state);
-            alert(`${this.state.inputText} has been added to the list!`); 
+            alert(`${this.state.inputText} has been added to the list!`);
         }
     };
 
     render() {
         return (
-            // TODO: make it broader, the hintText doesn't fit in + increase offset
-            <div className="col-sm-6 col-sm-offset-3 text-center">
+            <div className="text-center">
                 <TextField
                     hintText="Type in the city you wish to search for"
                     value={this.state.inputText}
                     onChange={this.handleChange}
                     onKeyDown={this.handleSubmit}
                 />
-
-
             </div>
         )
     };
