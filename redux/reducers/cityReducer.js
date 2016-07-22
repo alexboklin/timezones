41346197e137e027cb4b5eea496335beb95ba7cc
alexboklin <alex.boklin@gmail.com>
2016-07-23 00:09:58 +0300

    slightly reformat cityReducer

--- redux/reducers/cityReducer.js
+++ redux/reducers/cityReducer.js
@@ -3,24 +3,27 @@ import { ADD_CITY, DELETE_CITY } from '../actionTypes';
 function getId(cities) {
     return cities.reduce((maxId, city) => {
         return Math.max(city.id, maxId)
-    }, -1) + 1
+    }, -1) + 1;
 }
 
-let cityReducer = function(cities = [], action) {
-  switch (action.type) {
-    case ADD_CITY:
-      return [{
-          cityName: action.text,
-          enlisted: true,
-          id: getId(cities)
-        }, ...cities];
-    case DELETE_CITY:
-      return cities.filter((city) => {
-        return city.id !== action.id
-      });
-    default: 
-      return cities;
-  }
+let cityReducer = (cities = [], action) => {
+    switch (action.type) {
+        case ADD_CITY:
+            return [
+                ...cities,
+                {
+                    cityName: action.payload.text,
+                    enlisted: true,
+                    id: getId(cities)
+                }
+            ];
+        case DELETE_CITY:       
+            return cities.filter((city) => {
+                return city.id !== action.id;
+            });
+        default:
+            return cities;
+    }
 };
 
 export default cityReducer
