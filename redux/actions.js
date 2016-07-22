aff6dfa16c5081a1ed395272ecfe995a0bdb4441
alexboklin <alex.boklin@gmail.com>
2016-07-22 19:16:42 +0300

    use payloads in action creators

--- redux/actions.js
+++ redux/actions.js
@@ -5,12 +5,16 @@ export const DELETE_CITY = 'DELETE_CITY';
 // Wrap returned object in parens so it's interpreted as an object expression and not a block of code.
 export const addCity = (text) => ({
     type: ADD_CITY,
-    cityName: text
+    payload: {
+        cityName: text
+    }
 });
 
 export const deleteCity = (id) => ({
     type: DELETE_CITY,
-    id: id
+    payload: {
+        id: id
+    }
 });
 
 
