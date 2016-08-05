0b4fd513384e07521d9ec4782bf6fdd2cecb4936
alexboklin <alex.boklin@gmail.com>
2016-08-05 08:28:07 +0300

    Get rid of getId helper

--- redux/reducers/cities.js
+++ redux/reducers/cities.js
@@ -1,18 +1,12 @@
 import { ADD_CITY, DELETE_CITY, RESTORE_LAST_DELETED_CITY } from '../actionTypes';
 
-function getId(cities) {
-    return cities.reduce((maxId, city) => {
-        return Math.max(city.id, maxId)
-    }, -1) + 1;
-}
-
 let cities = (cities = [], action) => {
     switch (action.type) {
         case ADD_CITY:
             return [
                 ...cities,
                 {
-                    id: getId(cities),
+                    id: cities.reduce((maxId, city) => Math.max(city.id, maxId), -1) + 1,
                     name: action.payload.name,
                     enlisted: true // TODO: do we need this field (ids work just fine)?
                 }
