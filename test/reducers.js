d5bb982795f3d6d8be40871002e0341103b55f62
alexboklin <alex.boklin@gmail.com>
2016-08-06 22:24:35 +0300

    Add freezing to reducers' tests via Object.freeze

--- test/reducers.js
+++ test/reducers.js
@@ -11,8 +11,10 @@ describe('city reducer', () => {
         )
     });
     it('should add a new city to the list', () => {
+        const cityListBefore = [];
+        Object.freeze(cityListBefore);
         expect(
-            cities([], {
+            cities(cityListBefore, {
                 type: ADD_CITY,
                 payload: {
                     name: 'NYC'
@@ -27,25 +29,26 @@ describe('city reducer', () => {
             ]
         )
     });
-
     // TODO: sort out the problem with 0, 1, 2 turning to 0, 2 when removing id 1!
     it('should remove the city from the list by id', () => {
+        const cityListBefore = [
+            {
+                id: 0,
+                name: 'NYC'
+            },
+            {
+                id: 1,
+                name: 'LA'
+            },
+            {
+                id: 2,
+                name: 'Frisco'
+            }
+        ];
+        Object.freeze(cityListBefore);
         expect(
             cities(
-                [
-                    {
-                        id: 0,
-                        name: 'NYC'
-                    },
-                    {
-                        id: 1,
-                        name: 'LA'
-                    },
-                    {
-                        id: 2,
-                        name: 'Frisco'
-                    }
-                ],
+                cityListBefore,
                 {
                     type: DELETE_CITY,
                     payload: {
@@ -65,18 +68,20 @@ describe('city reducer', () => {
         ])
     });
     it('should restore the last deleted city', () => {
+        const cityListBefore = [
+            {
+                id: 0,
+                name: 'NYC'
+            },
+            {
+                id: 2,
+                name: 'Frisco'
+            }
+        ];
+        Object.freeze(cityListBefore);
         expect(
             cities(
-                [
-                    {
-                        id: 0,
-                        name: 'NYC'
-                    },
-                    {
-                        id: 2,
-                        name: 'Frisco'
-                    }
-                ],
+                cityListBefore,
                 {
                     type: RESTORE_LAST_DELETED_CITY,
                     payload: {
