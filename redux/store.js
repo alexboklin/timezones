a0fa1b3b4535b9daf99995a4e06f24eee5af0fa6
bapjiws <alex.boklin@gmail.com>
2016-09-20 19:58:11 +0300

    Add redux-thunk to the mix

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,13 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import rootReducer from './reducers';
 import logger from 'redux-logger';
-// import thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 
 let finalCreateStore = compose(
-  applyMiddleware(logger()) // + applyMiddleware(thunk),
+  applyMiddleware(
+      logger(),
+      thunk
+  )
 )(createStore);
 
 export default function configureStore(initialState = { cities: [] }) {
