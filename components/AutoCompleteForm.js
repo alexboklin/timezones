c71ed44d7dc7102dece20166db9b61e78945f003
bapjiws <alex.boklin@gmail.com>
2016-10-07 00:50:31 +0300

    Clean the code

--- components/AutoCompleteForm.js
+++ components/AutoCompleteForm.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { AutoComplete } from 'redux-form-material-ui';
 import { AutoComplete as MUIAutoComplete } from 'material-ui';
-import MenuItem from 'material-ui/MenuItem';
 import { Field, reduxForm } from 'redux-form';
-
-const dataSourceConfig = {
-    text: 'textKey',
-    value: 'valueKey',
-};
 
 const validate = values => {
     const errors = {};
-    const requiredFields = [ 'citySuggestion' ];
+    const requiredFields = ['citySuggestion'];
     requiredFields.forEach(field => {
-        if (!values[ field ]) {
-            errors[ field ] = 'Required'
+        if (!values[field]) {
+            errors[field] = 'Required';
         }
     });
 
+    // See: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/RegExp
     if (values.citySuggestion && !/^[A-Za-z\s]{1,},[A-Za-z\s]{1,}$/.test(values.citySuggestion)) {
-        errors.citySuggestion = 'City and country name separated by comma'
+        errors.citySuggestion = 'City and country name separated by comma';
     }
     return errors
 };
@@ -41,21 +36,8 @@ class AutoCompleteForm extends React.Component {
         this.props.citySuggestionsActions.fetchCitySuggestions(searchText);
     };
 
-    // handleNewRequest = (chosenRequest, index) => {
-    //     console.log("chosenRequest", chosenRequest);
-    //     console.log("index", index);
-    //
-    //     // TODO: deal with Snackbar component
-    //     // this.setState({
-    //     //     snackBarIsOpen: true,
-    //     //     snackBarMessage: `${chosenRequest} was added to the list`
-    //     // });
-    //
-    //     this.props.cityListActions.addLocationAndItsLocalTime(this.props.citySuggestions[index].id, chosenRequest);
-    // };
-
     // See https://github.com/erikras/redux-form/issues/190 for solution
-    // Returns an object where
+    // TODO: now can remove onNewRequest from the Field
     handleNewRequest = data => {
         console.log('HERE: ', data);
 
@@ -69,40 +51,12 @@ class AutoCompleteForm extends React.Component {
         this.props.cityListActions.addLocationAndItsLocalTime(citySuggestionWithId.id);
     };
 
-
-    // renderAutoComplete = () => (
-    //     <Autocomplete
-    //         filter={AutoComplete.caseInsensitiveFilter}
-    //         floatingLabelText="Type the city -- case insensitive"
-    //         dataSource={this.props.citySuggestions.map(suggestion => suggestion.text)}
-    //         onNewRequest={this.handleNewRequest}
-    //         onUpdateInput={this.handleUpdateInput}
-    //     />
-    // );
-
-// <form>
-// <Field name="citySuggester" component={this.renderAutoComplete}/>
-// </form>
-
     // See: http://erikras.github.io/redux-form-material-ui/
     // Also see: http://redux-form.com/6.0.5/examples/selectingFormValues/
     render() {
-        const { handleSubmit, pristine, reset, submitting } = this.props;
-
-        let suggestions = this.props.citySuggestions.map( suggestion => ({
-            text: suggestion.text,
-            value:  (
-                <MenuItem primaryText="text" value={suggestion}/>
-            )
-        }));
-
-        let suggestions2 = this.props.citySuggestions.map( suggestion => ({
-            textKey: suggestion.text,
-            valueKey: suggestion.id
-        }));
+        const { handleSubmit, pristine, submitting } = this.props;
 
         return (
-            // .map(suggestion => suggestion.text)
             // See http://redux-form.com/6.0.5/docs/api/Props.md/
             <form onSubmit={handleSubmit(this.handleNewRequest)}>
                 <div>
@@ -112,7 +66,6 @@ class AutoCompleteForm extends React.Component {
                         filter={MUIAutoComplete.caseInsensitiveFilter}
                         floatingLabelText="Type the city -- case insensitive"
                         dataSource={this.props.citySuggestions.map(suggestion => suggestion.text)}
-                        dataSourceConfig={dataSourceConfig}
                         onNewRequest={this.handleNewRequest}
                         onUpdateInput={this.handleUpdateInput}
                     />
