80998fd9d94e41198f82ee59f87e3987c3a24543
bapjiws <alex.boklin@gmail.com>
2016-10-10 21:45:29 +0300

    Improve error handling in addCityToListAndNotify

--- redux/actions/cities/list.js
+++ redux/actions/cities/list.js
@@ -95,7 +95,19 @@ export const addCityToListAndNotify = id => {
                 })
             }
         )
-        .catch(error => console.log(`Couldn't add location and its local time: ${error}`));
+        .catch(error => {
+            if (error.response) {
+                // The request was made, but the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+        });
     }
 };
 
