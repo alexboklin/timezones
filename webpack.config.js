9dece48e8f4c7b7edeeac45192a9191c6ecba318
bapjiws <alex.boklin@gmail.com>
2017-02-12 21:37:24 +0300

    Split app and vendor code in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,56 @@
 const webpack = require('webpack');
-
+const path = require('path');
 const dotEnvVars = require('dotenv').config();
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV;
+const inProductionMode = NODE_ENV === 'production';
+
 const envVars = Object.keys(dotEnvVars).
 	reduce( (acc, key) => {
 		acc[`__${key.toUpperCase()}__`] = JSON.stringify(dotEnvVars[key]);
 		return acc;
 }, {
-	__NODE_ENV__: JSON.stringify(process.env.NODE_ENV)
+	__NODE_ENV__: JSON.stringify(NODE_ENV)
 });
 
-const NODE_ENV = process.env.NODE_ENV;
-const inProductionMode = NODE_ENV === 'production';
+const plugins = [
+	new webpack.DefinePlugin(envVars),
+	new HtmlWebpackPlugin({
+		template: path.join(__dirname, '/app/index.html')
+	})
+];
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-	template: __dirname + '/app/index.html',
-	filename: 'index.html',
-	inject: 'body'
-});
+if (inProductionMode) {
+	plugins.push(
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ['vendor', 'manifest']
+		})
+	)
+} else {
+	plugins.push(
+		// TODO: [HMR] Consider using the NamedModulesPlugin for module names.
+		new webpack.HotModuleReplacementPlugin() // <-- To generate hot update chunks
+	)
+}
 
 /*On how to use Hot Module Replacement + React Hot Loader, see:
 https://medium.com/@rajaraodv/webpacks-hmr-react-hot-loader-the-missing-manual-232336dc0d96#.npgb2r5nn
 http://gaearon.github.io/react-hot-loader/getstarted/*/
 module.exports = {
 	devtool: inProductionMode ? undefined : 'cheap-module-eval-source-map',
-	entry: [
+	entry: inProductionMode ? {
+        bundle: './app/index.js',
+		// TODO: remove redux-logger -- unused.
+        vendor: ['react', 'react-dom', 'react-redux', 'redux', 'redux-logger', 'redux-persist', 'redux-thunk']
+    } : [
 		'webpack-dev-server/client?http://localhost:8080', // <-- Enables websocket connection (needs url and port)
 		'webpack/hot/only-dev-server', // <-- to perform HMR in the browser; "only" prevents reload on syntax errors
 		'./app/index.js' // The appʼs entry point
 	],
 	output: {
-		path: __dirname + '/build',
-		filename: 'bundle.js'
+		path: path.join(__dirname, 'build'),
+		filename: inProductionMode ? '[name].[chunkhash].js' : 'bundle.js'
 	},
 	module: {
 		rules: [
@@ -46,12 +65,7 @@ module.exports = {
     resolve: {
         extensions: ['.json', '.jsx', '.js']
     },
-	plugins: [
-	    HtmlWebpackPluginConfig,
-        new webpack.DefinePlugin(envVars),
-		new webpack.HotModuleReplacementPlugin() // <-- To generate hot update chunks
-        // TODO: [HMR] Consider using the NamedModulesPlugin for module names.
-    ],
+	plugins,
 	devServer: {
 		hot: true, // <-- Enables HMR in webpack-dev-server and in libs running in the browser
 		contentBase: './app'
