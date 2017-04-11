cf23f9cf4eb4b7e1c2fd270bef4fd61412891c3f
bapjiws <alex.boklin@gmail.com>
2017-04-12 01:34:39 +0300

    Destructure city and placeInList from action in city list reducers

--- app/redux/reducers/cities/list.js
+++ app/redux/reducers/cities/list.js
@@ -5,29 +5,29 @@ import {
 } from '../../actions/actionTypes';
 
 let cityList = (cityList = [], action) => {
-    // TODO: destructure city and placeInList from action.
+    const { city, placeInList } = action;
 
     switch (action.type) {
 
         case ADD_CITY:
             return [
                 ...cityList,
-                {...action.city, placeInList: cityList.reduce((maxId, city) => Math.max(city.placeInList, maxId), -1) + 1}
+                {...city, placeInList: cityList.reduce((maxId, city) => Math.max(city.placeInList, maxId), -1) + 1}
             ];
 
         case DELETE_CITY_BY_ITS_PLACE:
             return [
-                ...cityList.slice(0, action.placeInList),
-                ...cityList.slice(action.placeInList + 1).map(
+                ...cityList.slice(0, placeInList),
+                ...cityList.slice(placeInList + 1).map(
                     city => ({...city, placeInList: city.placeInList-1})
                 )
             ];
 
         case RESTORE_DELETED_CITY:
             return [
-                ...cityList.slice(0, action.city.placeInList),
-                action.city,
-                ...cityList.slice(action.city.placeInList).map(
+                ...cityList.slice(0, city.placeInList),
+                city,
+                ...cityList.slice(city.placeInList).map(
                     city => ({...city, placeInList: city.placeInList+1})
                 )
             ];
